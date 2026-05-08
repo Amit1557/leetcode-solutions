d6d53cdd4b243e7aefaf246a46e91568595b2e08
Amit Kumar <amit575124@gmail.com>
2026-05-08 23:58:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (38.90%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+         unordered_map<char, int> mp;
+
+        
+        for(char ch : s) {
+            mp[ch]++;
+        }
+
+        int ans = 0;
+        bool oddFound = false;
+
+        for(auto it : mp) {
+            int freq = it.second;
+
+            if(freq % 2 == 0) {
+                ans += freq;
+            }
+            else {
+                ans += freq - 1; 
+                oddFound = true;
+            }
+        }
+
+        
+        if(oddFound) ans++;
+
+        return ans;
+    }
+};
