9a760566dd25f75cc8ab915a709b7a1033ef1d3b
Amit Kumar <amit575124@gmail.com>
2026-07-03 22:40:55 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (6.95%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int total=numBottles;
+        
+        while(numBottles>=numExchange){
+            
+               total+=1;
+                numBottles=numBottles-numExchange+1;
+         
+        }
+        return total;
+    }
+};
