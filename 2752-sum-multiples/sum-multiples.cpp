787af4f99d31e11019e6eeef4e365ca05acad232
Amit Kumar <amit575124@gmail.com>
2026-07-04 22:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (45.83%) - LeetSync

---
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum=0;
+        while(n>=1){
+            if(n%3==0 || n%5==0 || n%7==0){
+               
+                sum+=n;
+                 n-=1;
+            }
+            else{
+                n-=1;
+            }
+        }
+        return sum;
+    }
+};
