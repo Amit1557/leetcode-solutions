158e15593d946afd59d45df520bc1d0268731e1e
Amit Kumar <amit575124@gmail.com>
2026-08-20 16:43:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (97.43%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void solve(vector<int> &nums, int i, vector<int> &temp, vector<vector<int>> &res) {
-        res.push_back(temp);
+    void solve(vector<int> &nums, int i, int temp[], int &count, vector<vector<int>> &res) {
+        res.emplace_back(temp, temp + count);      // build result vector directly, no extra copy step
         for (int idx = i; idx < (int)nums.size(); idx++) {
             if (idx > i && nums[idx] == nums[idx - 1]) continue;
-            temp.push_back(nums[idx]);
-            solve(nums, idx + 1, temp, res);
-            temp.pop_back();
+            temp[count++] = nums[idx];              // stack array, no heap alloc
+            solve(nums, idx + 1, temp, count, res);
+            count--;                                // backtrack
         }
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<int> temp;
-        temp.reserve(nums.size());          // avoid repeated reallocation
+        int temp[10];        // constraint: nums.length <= 10
+        int count = 0;
         vector<vector<int>> res;
-        res.reserve(1 << nums.size());      // exact upper bound, no vector doubling overhead
-        solve(nums, 0, temp, res);
+        res.reserve(1 << nums.size());
+        solve(nums, 0, temp, count, res);
         return res;
     }
 };
