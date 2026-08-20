980937245a007012f25c1bd9404390465ef9a48d
Amit Kumar <amit575124@gmail.com>
2026-08-20 16:42:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (99.89%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     void solve(vector<int> &nums, int i, vector<int> &temp, vector<vector<int>> &res) {
-    res.push_back(temp);
-    if(i == nums.size()) {
-        
-        return;
+        res.push_back(temp);
+        for (int idx = i; idx < (int)nums.size(); idx++) {
+            if (idx > i && nums[idx] == nums[idx - 1]) continue;
+            temp.push_back(nums[idx]);
+            solve(nums, idx + 1, temp, res);
+            temp.pop_back();
+        }
     }
-    for(int idx = i; idx < nums.size(); idx++) {
-        if(idx > i && nums[idx] == nums[idx-1]) continue;
-        temp.push_back(nums[idx]);
-        solve(nums, idx+1, temp, res);
-        temp.pop_back();
-    }
-}
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int i =0;
-    vector<int> temp;
-    vector<vector<int>> res;
-    sort(nums.begin(), nums.end());
-    solve(nums, i, temp, res);
-    return res;
+        sort(nums.begin(), nums.end());
+        vector<int> temp;
+        temp.reserve(nums.size());          // avoid repeated reallocation
+        vector<vector<int>> res;
+        res.reserve(1 << nums.size());      // exact upper bound, no vector doubling overhead
+        solve(nums, 0, temp, res);
+        return res;
     }
 };
