194c5a17c56060f508049e71f8e03634d8cb4cd0
Amit Kumar <amit575124@gmail.com>
2026-08-20 16:44:04 +0530

    Time: 5 ms (8.45%) | Memory: 17.5 MB (5.08%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    void solve(vector<int> &nums, int i, int temp[], int &count, vector<vector<int>> &res) {
-        res.emplace_back(temp, temp + count);      // build result vector directly, no extra copy step
-        for (int idx = i; idx < (int)nums.size(); idx++) {
-            if (idx > i && nums[idx] == nums[idx - 1]) continue;
-            temp[count++] = nums[idx];              // stack array, no heap alloc
-            solve(nums, idx + 1, temp, count, res);
-            count--;                                // backtrack
+    set<vector<int>> ans;
+    void subset(vector<int> &nums, int n, int indx, vector<int> temp){
+        if(indx == n){
+            sort(temp.begin(), temp.end());
+            ans.insert(temp);
+            return ;
         }
+        subset(nums , n, indx+1, temp);
+        temp.push_back(nums[indx]);
+        subset(nums, n, indx+1, temp);
     }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int temp[10];        // constraint: nums.length <= 10
-        int count = 0;
-        vector<vector<int>> res;
-        res.reserve(1 << nums.size());
-        solve(nums, 0, temp, count, res);
-        return res;
+        subset(nums, nums.size(), 0, {});
+        vector<vector<int>> temp(ans.begin(), ans.end());
+                                    return temp;
     }
 };
