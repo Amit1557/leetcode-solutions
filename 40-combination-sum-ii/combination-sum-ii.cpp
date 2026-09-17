f1790043cbdc4348651468790620ee80b567774f
Amit Kumar <amit575124@gmail.com>
2026-09-17 09:40:50 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (63.87%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -8,7 +8,7 @@ void find(int ind ,vector<int>& candidates, int target,vector<vector<int>>& ans 
         
    for(int  i = ind ; i < candidates.size() ; i++){
     if(i > ind  && candidates[i]==candidates[i-1]) continue;
-    if(candidates[i]>target) break;
+    if(candidates[ind]>target) break;
         ds.push_back(candidates[i]);
         find(i+1,candidates , target-candidates[i], ans , ds);
         ds.pop_back();
